688b0d3a6e972432446b9b9d8dca5b8484c55d83
Emanuel Freitas <efreitasn@hotmail.com>
2019-09-02 23:07:10 -0300

    Add fg and bg color attrs

--- customo.go
+++ customo.go
@@ -1,6 +1,7 @@
 package customo
 
 import (
+	"strconv"
 	"strings"
 )
 
@@ -12,9 +13,53 @@ const (
 	AttrCrossed
 )
 
+// Background colors
+const (
+	Bg8BitBlack         = 40
+	Bg8BitBrightBlack   = 100
+	Bg8BitRed           = 41
+	Bg8BitBrightRed     = 101
+	Bg8BitGreen         = 42
+	Bg8BitBrightGreen   = 102
+	Bg8BitYellow        = 43
+	Bg8BitBrightYellow  = 103
+	Bg8BitBlue          = 44
+	Bg8BitBrightBlue    = 104
+	Bg8BitMagenta       = 45
+	Bg8BitBrightMagenta = 105
+	Bg8BitCyan          = 46
+	Bg8BitBrightCyan    = 106
+	Bg8BitWhite         = 47
+	Bg8BitBrightWhite   = 107
+)
+
+// Foreground colors
+const (
+	Fg8BitBlack         = 30
+	Fg8BitBrightBlack   = 90
+	Fg8BitRed           = 31
+	Fg8BitBrightRed     = 91
+	Fg8BitGreen         = 32
+	Fg8BitBrightGreen   = 92
+	Fg8BitYellow        = 33
+	Fg8BitBrightYellow  = 93
+	Fg8BitBlue          = 34
+	Fg8BitBrightBlue    = 94
+	Fg8BitMagenta       = 35
+	Fg8BitBrightMagenta = 95
+	Fg8BitCyan          = 36
+	Fg8BitBrightCyan    = 96
+	Fg8BitWhite         = 37
+	Fg8BitBrightWhite   = 97
+)
+
 // C is the representation of attributes to be used in a string.
 type C struct {
 	attrs int
+	bg    int
+	fg    int
+	bgSet bool
+	fgSet bool
 }
 
 // New returns a new representation of attributes to be used in a string.
@@ -27,11 +72,23 @@ func (c *C) AddAttrs(attrs int) {
 	c.attrs |= attrs
 }
 
+// SetBg8BitAttr sets the background attribute using an 8-bit color
+func (c *C) SetBg8BitAttr(color int) {
+	c.bgSet = true
+	c.bg = color
+}
+
+// SetFg8BitAttr sets the background attribute using an 8-bit color
+func (c *C) SetFg8BitAttr(color int) {
+	c.fgSet = true
+	c.fg = color
+}
+
 // FormatString formats str using the added attributes.
 func (c *C) FormatString(str string) string {
 	var b strings.Builder
 
-	if c.attrs == 0 {
+	if c.attrs == 0 && !c.bgSet && !c.fgSet {
 		return str
 	}
 
@@ -83,6 +140,26 @@ func (c *C) writeAttrs(b *strings.Builder) {
 		} else {
 			atLeastOneFlag = true
 			b.WriteString("9")
+		}
+	}
+
+	// Background color
+	if c.bgSet {
+		if atLeastOneFlag {
+			b.WriteString(";" + strconv.Itoa(c.bg))
+		} else {
+			atLeastOneFlag = true
+			b.WriteString(strconv.Itoa(c.bg))
+		}
+	}
+
+	// Foreground color
+	if c.fgSet {
+		if atLeastOneFlag {
+			b.WriteString("a;" + strconv.Itoa(c.fg))
+		} else {
+			atLeastOneFlag = true
+			b.WriteString(strconv.Itoa(c.fg))
 		}
 	}
 
--- customo_test.go
+++ customo_test.go
@@ -23,7 +23,7 @@ func TestC(t *testing.T) {
 			fmt.Sprintf("\x1b[3;5m%v\x1b[0m", normalStr),
 		},
 		{
-			"Without flags",
+			"No flags",
 			0,
 			normalStr,
 		},
@@ -34,6 +34,94 @@ func TestC(t *testing.T) {
 			c := New()
 
 			c.AddAttrs(test.flags)
+
+			str := c.FormatString(normalStr)
+
+			if str != test.str {
+				t.Errorf("got %v, want %v", escapeStr(str), escapeStr(test.str))
+			}
+		})
+	}
+}
+
+func TestBgColor(t *testing.T) {
+	normalStr := "a"
+	tests := []struct {
+		title   string
+		flags   int
+		bgColor int
+		str     string
+	}{
+		{
+			"Bold and crossed with blue bg",
+			AttrBold | AttrCrossed,
+			Bg8BitBlue,
+			fmt.Sprintf("\x1b[1;9;%vm%v\x1b[0m", Bg8BitBlue, normalStr),
+		},
+		{
+			"Italic and blink with yellow bg",
+			AttrItalic | AttrBlink,
+			Bg8BitYellow,
+			fmt.Sprintf("\x1b[3;5;%vm%v\x1b[0m", Bg8BitYellow, normalStr),
+		},
+		{
+			"No flags with red bg",
+			0,
+			Bg8BitRed,
+			fmt.Sprintf("\x1b[%vm%v\x1b[0m", Bg8BitRed, normalStr),
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.title, func(t *testing.T) {
+			c := New()
+
+			c.AddAttrs(test.flags)
+			c.SetBg8BitAttr(test.bgColor)
+
+			str := c.FormatString(normalStr)
+
+			if str != test.str {
+				t.Errorf("got %v, want %v", escapeStr(str), escapeStr(test.str))
+			}
+		})
+	}
+}
+
+func TestFgColor(t *testing.T) {
+	normalStr := "a"
+	tests := []struct {
+		title   string
+		flags   int
+		fgColor int
+		str     string
+	}{
+		{
+			"Bold and crossed with blue fg",
+			AttrBold | AttrCrossed,
+			Fg8BitBlue,
+			fmt.Sprintf("\x1b[1;9;%vm%v\x1b[0m", Fg8BitBlue, normalStr),
+		},
+		{
+			"Italic and blink with yellow fg",
+			AttrItalic | AttrBlink,
+			Fg8BitYellow,
+			fmt.Sprintf("\x1b[3;5;%vm%v\x1b[0m", Fg8BitYellow, normalStr),
+		},
+		{
+			"No flags with red fg",
+			0,
+			Fg8BitRed,
+			fmt.Sprintf("\x1b[%vm%v\x1b[0m", Fg8BitRed, normalStr),
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.title, func(t *testing.T) {
+			c := New()
+
+			c.AddAttrs(test.flags)
+			c.SetFg8BitAttr(test.fgColor)
 
 			str := c.FormatString(normalStr)
 

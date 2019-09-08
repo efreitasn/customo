fcb15f11d203c1a324ac1dcbf004feee35496733
Emanuel Freitas <efreitasn@hotmail.com>
2019-09-08 01:26:01 -0300

    Update API

--- customo.go
+++ customo.go
@@ -1,179 +1,73 @@
 package customo
 
 import (
-	"errors"
 	"strconv"
 	"strings"
 )
 
-// These attributes are added to the outputted string in the order below.
+// Attributes
 const (
-	AttrBold = 1 << iota
-	AttrItalic
-	AttrBlink
-	AttrCrossed
+	AttrBold                = 1
+	AttrItalic              = 3
+	AttrBlink               = 5
+	AttrCrossed             = 9
+	AttrFg4BitBlack         = 30
+	AttrFg4BitRed           = 31
+	AttrFg4BitGreen         = 32
+	AttrFg4BitYellow        = 33
+	AttrFg4BitBlue          = 34
+	AttrFg4BitMagenta       = 35
+	AttrFg4BitCyan          = 36
+	AttrFg4BitWhite         = 37
+	AttrBg4BitBlack         = 40
+	AttrBg4BitRed           = 41
+	AttrBg4BitGreen         = 42
+	AttrBg4BitYellow        = 43
+	AttrBg4BitBlue          = 44
+	AttrBg4BitMagenta       = 45
+	AttrBg4BitCyan          = 46
+	AttrBg4BitWhite         = 47
+	AttrFg4BitBrightBlack   = 90
+	AttrFg4BitBrightRed     = 91
+	AttrFg4BitBrightGreen   = 92
+	AttrFg4BitBrightYellow  = 93
+	AttrFg4BitBrightBlue    = 94
+	AttrFg4BitBrightMagenta = 95
+	AttrFg4BitBrightCyan    = 96
+	AttrFg4BitBrightWhite   = 97
+	AttrBg4BitBrightBlack   = 100
+	AttrBg4BitBrightRed     = 101
+	AttrBg4BitBrightGreen   = 102
+	AttrBg4BitBrightYellow  = 103
+	AttrBg4BitBrightBlue    = 104
+	AttrBg4BitBrightMagenta = 105
+	AttrBg4BitBrightCyan    = 106
+	AttrBg4BitBrightWhite   = 107
 )
 
-// Foreground colors.
-const (
-	Fg4BitBlack         = 30
-	Fg4BitBrightBlack   = 90
-	Fg4BitRed           = 31
-	Fg4BitBrightRed     = 91
-	Fg4BitGreen         = 32
-	Fg4BitBrightGreen   = 92
-	Fg4BitYellow        = 33
-	Fg4BitBrightYellow  = 93
-	Fg4BitBlue          = 34
-	Fg4BitBrightBlue    = 94
-	Fg4BitMagenta       = 35
-	Fg4BitBrightMagenta = 95
-	Fg4BitCyan          = 36
-	Fg4BitBrightCyan    = 96
-	Fg4BitWhite         = 37
-	Fg4BitBrightWhite   = 97
-)
-
-// Background colors.
-const (
-	Bg4BitBlack         = 40
-	Bg4BitBrightBlack   = 100
-	Bg4BitRed           = 41
-	Bg4BitBrightRed     = 101
-	Bg4BitGreen         = 42
-	Bg4BitBrightGreen   = 102
-	Bg4BitYellow        = 43
-	Bg4BitBrightYellow  = 103
-	Bg4BitBlue          = 44
-	Bg4BitBrightBlue    = 104
-	Bg4BitMagenta       = 45
-	Bg4BitBrightMagenta = 105
-	Bg4BitCyan          = 46
-	Bg4BitBrightCyan    = 106
-	Bg4BitWhite         = 47
-	Bg4BitBrightWhite   = 107
-)
-
-// C is the representation of attributes to be used in a string.
-type C struct {
-	attrs int
-	bg    int
-	fg    int
-}
-
-// New returns a new representation of attributes to be used in a string.
-// In order to no foreground color to be applied, pass 0 to fg4Bit. The same goes to bg4Bit and attrs.
-func New(fg4Bit, bg4Bit, attrs int) (*C, error) {
-	// Foreground color checks.
-	fg4BitWithinNormalRange := (fg4Bit == 0 || (fg4Bit >= Fg4BitBlack && fg4Bit <= Fg4BitWhite))
-	fg4BitWithinBrightRange := (fg4Bit == 0 || (fg4Bit >= Fg4BitBrightBlack && fg4Bit <= Fg4BitBrightWhite))
-
-	if !(fg4BitWithinNormalRange || fg4BitWithinBrightRange) {
-		return nil, errors.New("fg4Bit not within the allowed range")
-	}
-
-	// Background color checks
-	bg4BitWithinNormalRange := (bg4Bit == 0 || (bg4Bit >= Bg4BitBlack && bg4Bit <= Bg4BitWhite))
-	bg4BitWithinBrightRange := (bg4Bit == 0 || (bg4Bit >= Bg4BitBrightBlack && bg4Bit <= Bg4BitBrightWhite))
-
-	if !(bg4BitWithinNormalRange || bg4BitWithinBrightRange) {
-		return nil, errors.New("bg4Bit not within the allowed range")
-	}
-
-	return &C{
-		fg:    fg4Bit,
-		bg:    bg4Bit,
-		attrs: attrs,
-	}, nil
-}
-
-// MustNew executes New and panics if it returns an error.
-func MustNew(fg4Bit, bg4Bit, attrs int) *C {
-	c, err := New(fg4Bit, bg4Bit, attrs)
-
-	if err != nil {
-		panic(err)
-	}
-
-	return c
-}
-
-// FormatString formats str using the added attributes.
-func (c *C) FormatString(str string) string {
+// Format formats str using the added attributes.
+func Format(str string, attrs ...int) string {
 	var b strings.Builder
 
-	if c.attrs == 0 && c.bg == 0 && c.fg == 0 {
+	if len(attrs) == 0 {
 		return str
 	}
 
-	c.writeAttrs(&b)
+	writeAttrs(&b, attrs)
 	b.WriteString(str)
 	b.WriteString("\x1b[0m")
 
 	return b.String()
 }
 
-func (c *C) writeAttrs(b *strings.Builder) {
-	atLeastOneFlag := false
+func writeAttrs(b *strings.Builder, attrs []int) {
 	b.WriteString("\x1b[")
 
-	// Bold.
-	if c.attrs&AttrBold != 0 {
-		if atLeastOneFlag {
-			b.WriteString(";1")
-		} else {
-			atLeastOneFlag = true
-			b.WriteString("1")
-		}
-	}
+	for i, attr := range attrs {
+		b.WriteString(strconv.Itoa(attr))
 
-	// Italic.
-	if c.attrs&AttrItalic != 0 {
-		if atLeastOneFlag {
-			b.WriteString(";3")
-		} else {
-			atLeastOneFlag = true
-			b.WriteString("3")
-		}
-	}
-
-	// Blink.
-	if c.attrs&AttrBlink != 0 {
-		if atLeastOneFlag {
-			b.WriteString(";5")
-		} else {
-			atLeastOneFlag = true
-			b.WriteString("5")
-		}
-	}
-
-	// Crossed.
-	if c.attrs&AttrCrossed != 0 {
-		if atLeastOneFlag {
-			b.WriteString(";9")
-		} else {
-			atLeastOneFlag = true
-			b.WriteString("9")
-		}
-	}
-
-	// Foreground color.
-	if c.fg != 0 {
-		if atLeastOneFlag {
-			b.WriteString(";" + strconv.Itoa(c.fg))
-		} else {
-			atLeastOneFlag = true
-			b.WriteString(strconv.Itoa(c.fg))
-		}
-	}
-
-	// Background color.
-	if c.bg != 0 {
-		if atLeastOneFlag {
-			b.WriteString(";" + strconv.Itoa(c.bg))
-		} else {
-			atLeastOneFlag = true
-			b.WriteString(strconv.Itoa(c.bg))
+		if i != len(attrs)-1 {
+			b.WriteString(";")
 		}
 	}
 
--- customo_test.go
+++ customo_test.go
@@ -8,79 +8,53 @@ import (
 func TestC(t *testing.T) {
 	normalStr := "a"
 	tests := []struct {
-		title    string
-		attrs    int
-		fgColor  int
-		bgColor  int
-		str      string
-		errorNil bool
+		title string
+		attrs []int
+		str   string
 	}{
 		{
 			"Bold, crossed, bright, black fg and cyan bg",
-			AttrBold | AttrCrossed,
-			Fg4BitBlack,
-			Bg4BitBrightCyan,
+			[]int{
+				AttrBold,
+				AttrCrossed,
+				AttrFg4BitBlack,
+				AttrBg4BitBrightCyan,
+			},
 			fmt.Sprintf("\x1b[1;9;30;106m%v\x1b[0m", normalStr),
-			true,
 		},
 		{
 			"Italic, blink, bright black fg and blue bg",
-			AttrItalic | AttrBlink,
-			Fg4BitBrightBlack,
-			Bg4BitBlue,
+			[]int{
+				AttrItalic,
+				AttrBlink,
+				AttrFg4BitBrightBlack,
+				AttrBg4BitBlue,
+			},
 			fmt.Sprintf("\x1b[3;5;90;44m%v\x1b[0m", normalStr),
-			true,
 		},
 		{
 			"Bold, blink, yellow fg and white bg",
-			AttrBold | AttrBlink,
-			Fg4BitYellow,
-			Bg4BitWhite,
+			[]int{
+				AttrBold,
+				AttrBlink,
+				AttrFg4BitYellow,
+				AttrBg4BitWhite,
+			},
 			fmt.Sprintf("\x1b[1;5;33;47m%v\x1b[0m", normalStr),
-			true,
-		},
-		{
-			"Foreground color out of range",
-			0,
-			93811,
-			0,
-			"",
-			false,
-		},
-		{
-			"Background color out of range",
-			0,
-			0,
-			93811,
-			"",
-			false,
 		},
 		{
 			"No attrs",
-			0,
-			0,
-			0,
+			[]int{},
 			normalStr,
-			true,
 		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.title, func(t *testing.T) {
-			c, err := New(test.fgColor, test.bgColor, test.attrs)
+			str := Format(normalStr, test.attrs...)
 
-			if err == nil != test.errorNil {
-				if test.errorNil {
-					t.Errorf("got %v, want nil", err)
-				} else {
-					t.Errorf("got %v, want non-nil value", err)
-				}
-			} else if err == nil {
-				str := c.FormatString(normalStr)
-
-				if str != test.str {
-					t.Errorf("got %v, want %v", escapeStr(str), escapeStr(test.str))
-				}
+			if str != test.str {
+				t.Errorf("got %v, want %v", escapeStr(str), escapeStr(test.str))
 			}
 		})
 	}
